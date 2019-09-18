2adb7a675709b9674fcaf51fd2f71a1cf910bb6f
Scott Chen <48900010+FUTURETECH6@users.noreply.github.com>
2019-09-18 14:33:41 +0800

    Add files via upload

--- lab1/lab1test.c
+++ lab1/lab1test.c
@@ -5,15 +5,19 @@
 
 static clock_t start, stop;
 static clock_t ticks[8];
-int iternum[3] = {10000, 1000000, 1000000};
-double result;
+static double result;
+//the iteration times of a single alog func. Bigger the magnitude, higher the accuracy but also time consumption.
+static int iternum[3] = {
+    1000,
+    100000,
+    100000,
+};
 
 FILE *fp;
 
 void printResult(int mark);
 
-int main()
-{
+int main() {
     int aryN[8] = {
         1000,
         5000,
@@ -24,13 +28,12 @@ int main()
         80000,
         100000,
     };
-    fp = fopen("./testResult.md", "w");
+    fp = fopen("/Users/futuretech6/Documents/大二上/数结/HW/Project1/Prog/testResult.md", "w");
+
     fprintf(fp, "| | N |");
-    for (int i = 0; i < 8; i++)
-        fprintf(fp, " %d |", aryN[i]);
+    for (int i = 0; i < 8; i++) fprintf(fp, " %d |", aryN[i]);
     fprintf(fp, "\n|");
-    for (int i = 0; i < 10; i++)
-        fprintf(fp, " :--: |");
+    for (int i = 0; i < 10; i++) fprintf(fp, " :--: |");
     fprintf(fp, "\n");
 
     for (int i = 0; i < 8; i++) {
@@ -38,7 +41,7 @@ int main()
         for (int j = 0; j < iternum[0]; j++)
             result = PowClassic(1.0001, aryN[i]);
         stop = clock();
-        printf("%f\n", result);
+        printf("%g\n", result);
         ticks[i] = stop - start;
     }
     printResult(1);
@@ -48,7 +51,7 @@ int main()
         for (int j = 0; j < iternum[1]; j++)
             result = PowRecursive(1.0001, aryN[i]);
         stop = clock();
-        printf("%f\n", result);
+        printf("%g\n", result);
         ticks[i] = stop - start;
     }
     printResult(2);
@@ -58,37 +61,41 @@ int main()
         for (int j = 0; j < iternum[2]; j++)
             result = PowIterative(1.0001, aryN[i]);
         stop = clock();
-        printf("%f\n", result);
+        printf("%g\n", result);
         ticks[i] = stop - start;
     }
     printResult(3);
+
     fclose(fp);
     return 0;
 }
 
-void printResult(int mark)
-{
+void printResult(int mark) {
     fprintf(fp, "| Algorithm%d | Iterations(K)", mark);
     for (int i = 0; i < 8; i++) {
         fprintf(fp, " | %d", iternum[mark - 1]);
     }
 
-    fprintf(fp, " |\n| | Ticks");
+    switch (mark) {
+        case 1: fprintf(fp, " |\n| Classic | Ticks"); break;
+        case 2: fprintf(fp, " |\n| Recursive | Ticks"); break;
+        case 3: fprintf(fp, " |\n| Iterative | Ticks"); break;
+    }
 
     for (int i = 0; i < 8; i++) {
         fprintf(fp, " | %lu", ticks[i]); //clock_t is actually unsigned long
     }
-    fprintf(fp, " |\n| | Total Times(milisecond)");
+    fprintf(fp, " |\n| | Total Times(ms)");
 
     double totalTimes[8]; //Use array to save the total and save the times used in the division
     for (int i = 0; i < 8; i++) {
-        totalTimes[i] = (double)ticks[i];
-        fprintf(fp, " | %f", totalTimes[i]);
+        totalTimes[i] = (double)ticks[i] / CLK_TCK * 1000.0;
+        fprintf(fp, " | %g", totalTimes[i]);
     }
-    fprintf(fp, " |\n| | Duration(nanosecond)");
+    fprintf(fp, " |\n| | Duration(ns)");
 
     for (int i = 0; i < 8; i++) {
-        fprintf(fp, " | %f", (double)(totalTimes[i] * 1000000 / iternum[mark - 1]));
+        fprintf(fp, " | %g", (double)(totalTimes[i] * 1000000.0 / iternum[mark - 1]));
     }
     fprintf(fp, " |\n");
 }
