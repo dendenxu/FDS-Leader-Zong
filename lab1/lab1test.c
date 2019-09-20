b99b21ae444133815f26fd81a2882036f9a0d9db
xudenden <56295612@qq.com>
2019-09-20 23:28:37 +0800

    change 0 to loopFunc

--- lab1/lab1test.c
+++ lab1/lab1test.c
@@ -13,15 +13,15 @@ static int iternum[3] = {
     1000000,
     1000000,
 };
-#define testBase 1.0001  //def the datum used in the test
+#define testBase 1.0001 //def the datum used in the test
 
-FILE *fp;  //set a file pointer for the .md file
+FILE *fp; //set a file pointer for the .md file
 double (*pFunc[3])(double, int) = {PowClassic, PowRecursive, PowIterative};
 
+void printResult(int mark); //init the function to output the result to .md
 
-void printResult(int mark);  //init the function to output the result to .md
-
-int main() {
+int main()
+{
     int aryN[8] = {
         1000,
         5000,
@@ -32,22 +32,24 @@ int main() {
         80000,
         100000,
     };
-    fp = fopen("./testResult.md", "w");  //set the relative path and the file mode in "w" so that the old data can be deleted
-    fprintf(fp, "| | N |");  //print the title of the table
-    for (int i = 0; i < 8; i++) fprintf(fp, " %d |", aryN[i]);  //print the magnitude of the exponent
-    fprintf(fp, "\n|");  //print the title of the table
-    for (int i = 0; i < 10; i++) fprintf(fp, " :--: |");  //set the table to align center
+    fp = fopen("./testResult.md", "w"); //set the relative path and the file mode in "w" so that the old data can be deleted
+    fprintf(fp, "| | N |");             //print the title of the table
+    for (int i = 0; i < 8; i++)
+        fprintf(fp, " %d |", aryN[i]); //print the magnitude of the exponent
+    fprintf(fp, "\n|");                //print the title of the table
+    for (int i = 0; i < 10; i++)
+        fprintf(fp, " :--: |"); //set the table to align center
     fprintf(fp, "\n");
 
-    for (int loopFunc = 0; loopFunc < 3; loopFunc++) {  //loop foe 3 times to test the 3 algorithms
+    for (int loopFunc = 0; loopFunc < 3; loopFunc++) { //loop foe 3 times to test the 3 algorithms
         for (int i = 0; i < 8; i++) {
-            start = clock();  //start keep the time
-            for (int j = 0; j < iternum[0] - 1; j++)  //iterate iternum[0]-1 times to increase accuracy
+            start = clock();                                //start keep the time
+            for (int j = 0; j < iternum[loopFunc] - 1; j++) //iterate iternum[0]-1 times to increase accuracy
                 result = pFunc[loopFunc](testBase, aryN[i]);
-            result = pFunc[loopFunc](testBase, aryN[i]);  //run 1 time to get the result so that the time used to keep the data can be saved.
-            stop = clock();  //stop keep the time
-            printf("%g\n", result);  //print the result to the command line
-            ticks[i] = stop - start;  //keep the tick
+            result = pFunc[loopFunc](testBase, aryN[i]); //run 1 time to get the result so that the time used to keep the data can be saved.
+            stop = clock();                              //stop keep the time
+            printf("%g\n", result);                      //print the result to the command line
+            ticks[i] = stop - start;                     //keep the tick
         }
         printResult(loopFunc + 1); //give a index to func printResult(int) so that it can make the table more detailed
     }
@@ -55,18 +57,24 @@ int main() {
     return 0;
 }
 
-void printResult(int mark) {
+void printResult(int mark)
+{
     fprintf(fp, "| Algorithm%d | Iterations(K)", mark);
     for (int i = 0; i < 8; i++) {
         fprintf(fp, " | %d", iternum[mark - 1]);
     }
 
     switch (mark) {
-        case 1: fprintf(fp, " |\n| Classic | Ticks"); break;
-        case 2: fprintf(fp, " |\n| Recursive | Ticks"); break;
-        case 3: fprintf(fp, " |\n| Iterative | Ticks"); break;
+    case 1:
+        fprintf(fp, " |\n| Classic | Ticks");
+        break;
+    case 2:
+        fprintf(fp, " |\n| Recursive | Ticks");
+        break;
+    case 3:
+        fprintf(fp, " |\n| Iterative | Ticks");
+        break;
     }
-
 
     for (int i = 0; i < 8; i++) {
         fprintf(fp, " | %lu", ticks[i]); //clock_t is actually unsigned long
