8e1f126cab03f53231abd8c7842fcbe9d985d529
Scott Chen <48900010+FUTURETECH6@users.noreply.github.com>
2019-09-21 14:50:15 +0800

    Add more comments

--- lab1/lab1test.c
+++ lab1/lab1test.c
@@ -1,4 +1,4 @@
-#include "lab1algoimplementation.c"
+#include "lab1algoimplementation.c"  //get the func of 3 algorithms
 #include <time.h>
 
 #define CLK_TCK CLOCKS_PER_SEC
@@ -13,16 +13,16 @@ static int iternum[3] = {
     1000000,
     1000000,
 };
-#define testBase 1.0001 //def the datum used in the test
+#define testBase 1.0001  //def the datum used in the test
 
-FILE *fp; //set a file pointer for the .md file
+FILE *fp;  //set a file pointer for the .md file
 double (*pFunc[3])(double, int) = {PowClassic, PowRecursive, PowIterative};
 
-void printResult(int mark); //init the function to output the result to .md
 
-int main()
-{
-    int aryN[8] = {
+void printResult(int mark);  //init the function to output the result to .md
+
+int main() {
+    int aryN[8] = {  //set the array of exponent
         1000,
         5000,
         10000,
@@ -32,64 +32,56 @@ int main()
         80000,
         100000,
     };
-    fp = fopen("./testResult.md", "w"); //set the relative path and the file mode in "w" so that the old data can be deleted
-    fprintf(fp, "| | N |");             //print the title of the table
-    for (int i = 0; i < 8; i++)
-        fprintf(fp, " %d |", aryN[i]); //print the magnitude of the exponent
-    fprintf(fp, "\n|");                //print the title of the table
-    for (int i = 0; i < 10; i++)
-        fprintf(fp, " :--: |"); //set the table to align center
+    fp = fopen("testResult.md", "w");  //set the relative path and the file mode in "w" so that the old data can be deleted
+    fprintf(fp, "| | N |");  //print the title of the table
+    for (int i = 0; i < 8; i++) fprintf(fp, " %d |", aryN[i]);  //print the magnitude of the exponent
+    fprintf(fp, "\n|");  //print the title of the table
+    for (int i = 0; i < 10; i++) fprintf(fp, " :--: |");  //set the table to align center
     fprintf(fp, "\n");
 
-    for (int loopFunc = 0; loopFunc < 3; loopFunc++) { //loop foe 3 times to test the 3 algorithms
+    for (int loopFunc = 0; loopFunc < 3; loopFunc++) {  //loop foe 3 times to test the 3 algorithms
         for (int i = 0; i < 8; i++) {
-            start = clock();                                //start keep the time
-            for (int j = 0; j < iternum[loopFunc] - 1; j++) //iterate iternum[0]-1 times to increase accuracy
+            start = clock();  //start keep the time
+            for (int j = 0; j < iternum[loopFunc] - 1; j++)  //iterate iternum[0]-1 times to increase accuracy
                 result = pFunc[loopFunc](testBase, aryN[i]);
-            result = pFunc[loopFunc](testBase, aryN[i]); //run 1 time to get the result so that the time used to keep the data can be saved.
-            stop = clock();                              //stop keep the time
-            printf("%g\n", result);                      //print the result to the command line
-            ticks[i] = stop - start;                     //keep the tick
+            result = pFunc[loopFunc](testBase, aryN[i]);  //run 1 time to get the result so that the time used to keep the data can be saved.
+            stop = clock();  //stop keep the time
+            printf("%g\n", result);  //print the result to the command line
+            ticks[i] = stop - start;  //keep the tick
         }
-        printResult(loopFunc + 1); //give a index to func printResult(int) so that it can make the table more detailed
+        printResult(loopFunc + 1);  //give a index to func printResult(int) so that it can make the table more detailed
     }
     fclose(fp);
     return 0;
 }
 
-void printResult(int mark)
-{
-    fprintf(fp, "| Algorithm%d | Iterations(K)", mark);
+void printResult(int mark) {
+    fprintf(fp, "| Algorithm%d | Iterations(K)", mark);  //print the mark of the algorithms
     for (int i = 0; i < 8; i++) {
-        fprintf(fp, " | %d", iternum[mark - 1]);
+        fprintf(fp, " | %d", iternum[mark - 1]);  //print the iterations time
     }
 
-    switch (mark) {
-    case 1:
-        fprintf(fp, " |\n| Classic | Ticks");
-        break;
-    case 2:
-        fprintf(fp, " |\n| Recursive | Ticks");
-        break;
-    case 3:
-        fprintf(fp, " |\n| Iterative | Ticks");
-        break;
+    switch (mark) {  //print the algorithm name
+        case 1: fprintf(fp, " |\n| Classic | Ticks"); break;
+        case 2: fprintf(fp, " |\n| Recursive | Ticks"); break;
+        case 3: fprintf(fp, " |\n| Iterative | Ticks"); break;
     }
 
+
     for (int i = 0; i < 8; i++) {
-        fprintf(fp, " | %lu", ticks[i]); //clock_t is actually unsigned long
+        fprintf(fp, " | %lu", ticks[i]);  //clock_t is actually unsigned long
     }
     fprintf(fp, " |\n| | Total Times(ms)");
 
-    double totalTimes[8]; //Use array to save the total and save the times used in the division
+    double totalTimes[8];  //Use array to save the Total Times and save the time used in the division operations
     for (int i = 0; i < 8; i++) {
         totalTimes[i] = (double)ticks[i] / CLK_TCK * 1000.0;
-        fprintf(fp, " | %g", totalTimes[i]);
+        fprintf(fp, " | %g", totalTimes[i]);  //print Total Times
     }
     fprintf(fp, " |\n| | Duration(ns)");
 
     for (int i = 0; i < 8; i++) {
-        fprintf(fp, " | %g", (double)(totalTimes[i] * 1000000.0 / iternum[mark - 1]));
+        fprintf(fp, " | %g", (double)(totalTimes[i] * 1000000.0 / iternum[mark - 1]));  //print Duration of a single call
     }
     fprintf(fp, " |\n");
 }
