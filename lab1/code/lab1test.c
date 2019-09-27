c4540391761c2abd23468edb3198cadfd3201df2
xudenden <56295612@qq.com>
2019-09-27 23:37:19 +0800

    emm?

--- lab1/code/lab1test.c
+++ lab1/code/lab1test.c
@@ -1,17 +1,17 @@
 #include "lab1algoimplementation.c" //get the func of 3 algorithms
 #include <time.h>
 
-#define CLK_TCK CLOCKS_PER_SEC
+#define CLK_TCK CLOCKS_PER_SEC      //redef the macro
 
-static clock_t start, stop;
-static clock_t ticks[8];
+static clock_t start, stop;         //save the time used for ticks counting
+static clock_t ticks[8];            //use an array to save the ticks data for printing
 static double result;
 //the iteration times of a single alog func.
 //Bigger the magnitude, higher the accuracy but also time consumption.
 
-#define testBase 1.0001 //def the datum used in the test
+#define testBase 1.0001             //def the datum used in the test
 
-FILE *fp; //set a file pointer for the .md file
+FILE *fp;                           //set a file pointer for the .md file
 double (*pFunc[])(double, int) = {PowClassic, PowRecursive, PowIterative, PowIterativeBetter};
 static int iternum[sizeof(pFunc) / sizeof(pFunc[0])] = {
     1000,
@@ -34,13 +34,13 @@ int main()
         80000,
         100000,
     };
-    fp = fopen("testResult.md", "w"); //set the relative path and the file mode in "w" so that the old data can be deleted
-    fprintf(fp, "| | N |");           //print the title of the table
+    fp = fopen("testResult.md", "w");  //set the relative path and the file mode in "w" so that the old data can be deleted
+    fprintf(fp, "| | N |");            //print the title of the table
     for (int i = 0; i < 8; i++)
         fprintf(fp, " %d |", aryN[i]); //print the magnitude of the exponent
     fprintf(fp, "\n|");                //print the title of the table
     for (int i = 0; i < 10; i++)
-        fprintf(fp, " :--: |"); //set the table to align center
+        fprintf(fp, " :--: |");        //set the table to align center
     fprintf(fp, "\n");
 
     for (int loopFunc = 0; loopFunc < sizeof(pFunc) / sizeof(pFunc[0]); loopFunc++) { //loop for 3 times to test the 3 algorithms
@@ -63,33 +63,33 @@ void printResult(int mark)
 {
     fprintf(fp, "| Algorithm%d | Iterations(K)", mark); //print the mark of the algorithms
     for (int i = 0; i < 8; i++) {
-        fprintf(fp, " | %d", iternum[mark - 1]); //print the iterations time
+        fprintf(fp, " | %d", iternum[mark - 1]);        //print the iterations time
     }
 
-    switch (mark) { //print the algorithm name
-    case 1:
-        fprintf(fp, " |\n| Classic | Ticks");
-        break;
-    case 2:
-        fprintf(fp, " |\n| Recursive | Ticks");
-        break;
-    case 3:
-        fprintf(fp, " |\n| Iterative | Ticks");
-        break;
-    case 4:
-        fprintf(fp, " |\n| IterativeBetter | Ticks");
-        break;
+    switch (mark) {                                     //print the algorithm name
+        case 1:
+            fprintf(fp, " |\n| Classic | Ticks");
+            break;
+        case 2:
+            fprintf(fp, " |\n| Recursive | Ticks");
+            break;
+        case 3:
+            fprintf(fp, " |\n| Iterative | Ticks");
+            break;
+        case 4:
+            fprintf(fp, " |\n| IterativeBetter | Ticks");
+            break;
     }
 
     for (int i = 0; i < 8; i++) {
-        fprintf(fp, " | %lu", ticks[i]); //clock_t is actually unsigned long
+        fprintf(fp, " | %lu", ticks[i]);            //clock_t is actually unsigned long
     }
     fprintf(fp, " |\n| | Total Times(ms)");
 
-    double totalTimes[8]; //Use array to save the Total Times and save the time used in the division operations
+    double totalTimes[8];                           //Use array to save the Total Times and save the time used in the division operations
     for (int i = 0; i < 8; i++) {
         totalTimes[i] = (double)ticks[i] / CLK_TCK * 1000.0;
-        fprintf(fp, " | %g", totalTimes[i]); //print Total Times
+        fprintf(fp, " | %g", totalTimes[i]);        //print Total Times
     }
     fprintf(fp, " |\n| | Duration(ns)");
 
---
+++ lab1/readme.md
@@ -0,0 +1,19 @@
+# Lab1--Performance Measure(POW)
+
+We kindly leaked some personal information in the Peer Review version.
+
+Never do that again.
+
+Leave the `lab1pr.zip` as it is so that ever time we see it, we get warned.
+
+`code` folder contains the newest algorithm implementation and test implementation.
+
+`document` folder contains the newest PDF version of the project report.
+
+`Report Files` contains some assets of the project report.
+
+`lab1.zip` is the first submitted final version.
+
+`lab1improved.zip` is the second submitted final version(improved).
+
+`lab1pr.zip` is to warn us.
